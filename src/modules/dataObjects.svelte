179c86a6405ea0cd944555d036da9c460e3aa7d3
Levy Adams <letitslide87@gmail.com>
2020-07-16 19:45:53 -0400

    yaaa

--- src/modules/dataObjects.svelte
+++ src/modules/dataObjects.svelte
@@ -4,7 +4,33 @@ export const locations = [
     {
         name:'brownstown',
         cookieWeight: .5,
-        priceSpread:8,
+        priceSpread:9,
+        localPrices: [
+            {name:'semonas',price:0},
+            {name:'mintz',price:0},
+            {name:'shmores',price:0},
+            {name:'chipz',price:0},
+            {name:'wagalongs',price:0},
+            {name:'patties',price:0}
+        ]
+    },
+     {
+        name:'regsville',
+        cookieWeight: .75,
+        priceSpread:7,
+        localPrices: [
+            {name:'semonas',price:0},
+            {name:'mintz',price:0},
+            {name:'shmores',price:0},
+            {name:'chipz',price:0},
+            {name:'wagalongs',price:0},
+            {name:'patties',price:0}
+        ]
+    },
+     {
+        name:'midscity',
+        cookieWeight: 1,
+        priceSpread:4,
         localPrices: [
             {name:'semonas',price:0},
             {name:'mintz',price:0},
@@ -17,7 +43,7 @@ export const locations = [
     {
         name:'Le Pew',
         cookieWeight: 1.5,
-        priceSpread:8,
+        priceSpread:9,
         localPrices: [
             {name:'semonas',price:0},
             {name:'mintz',price:0},
@@ -33,23 +59,23 @@ export const cookies = [
     {
         name:'semonas',
         icon:'icon-spinner5',
-        minPrice:10,
-        maxPrice:700
+        minPrice:5,
+        maxPrice:100
     },
     {
         name:'mintz',
         icon:'icon-spinner5',
 
-        minPrice:1,
-        maxPrice:50
+        minPrice:3,
+        maxPrice:75
 
     },
     {
         name:'shmores',
         icon:'icon-spinner5',
 
-        minPrice:5,
-        maxPrice:100
+        minPrice:2,
+        maxPrice:65
 
     },
     {
@@ -57,20 +83,20 @@ export const cookies = [
         icon:'icon-spinner5',
 
         minPrice:1,
-        maxPrice:20
+        maxPrice:40
 
     },
     {
         name:'wagalongs',
         icon:'icon-spinner5',
-        minPrice:5,
-        maxPrice:60
+        minPrice:1,
+        maxPrice:50
     },
     {
         name:'patties',
         icon:'icon-spinner5',
-        minPrice:10,
-        maxPrice:200
+        minPrice:3,
+        maxPrice:80
     }
 
 ];
